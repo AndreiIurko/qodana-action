25b7166f3d0f0a55e18649ae743b5aec719eceeb
AndreiIurko <andreiiurko01@gmail.com>
2025-07-03 18:37:55 +0300

    Check everything except fixes

--- scan/integrationTests/src/github.mock.integration.ts
+++ scan/integrationTests/src/github.mock.integration.ts
@@ -1,54 +1,53 @@
-import * as sinon from 'sinon';
-import * as exec from '@actions/exec';
-import * as Context from '@actions/github/lib/context'; // Context type definition
-import * as artifact from '@actions/artifact';
-import * as cache from '@actions/cache';
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-import * as utils from '../../src/utils';
+import * as sinon from 'sinon'
+import * as exec from '@actions/exec'
+import * as Context from '@actions/github/lib/context' // Context type definition
+import * as artifact from '@actions/artifact'
+import * as cache from '@actions/cache'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
-export const execSpy = sinon.spy(exec, 'getExecOutput');
-// core.info("Started to initialise inputs spy:")
-// export const getInputsSpy = sinon.spy(utils, 'getInputs')
+export const execSpy = sinon.spy(exec, 'getExecOutput')
+export const cacheRestoreSpy = sinon.spy(cache, 'restoreCache')
 
 export function setupGithubArtifactMock(mockArtifactUpload: sinon.SinonStub) {
-  sinon.stub(artifact.default, 'uploadArtifact').callsFake(mockArtifactUpload);
+  sinon.stub(artifact.default, 'uploadArtifact').callsFake(mockArtifactUpload)
 }
 
-export function setupGithubCacheMock(
-  mockCacheRestore: sinon.SinonStub,
-  mockCacheSave: sinon.SinonStub,
+export function setupGithubCoreMock(
+  mockSummaryWrite: sinon.SinonStub | undefined,
+  setFailed: sinon.SinonStub<[message: string | Error], void> | undefined
 ) {
-  sinon.stub(cache, 'restoreCache').callsFake(mockCacheRestore);
-  sinon.stub(cache, 'saveCache').callsFake(mockCacheSave);
-}
-
-export function setupGithubCoreMock(mockSummaryWrite: sinon.SinonStub) {
-  const mockSummaryAddRaw = sinon.stub();
-  mockSummaryAddRaw.returnsThis();
-  sinon.stub(core.summary, 'addRaw').callsFake(mockSummaryAddRaw);
-  sinon.stub(core.summary, 'write').callsFake(mockSummaryWrite);
+  if (mockSummaryWrite != undefined) {
+    const mockSummaryAddRaw = sinon.stub()
+    mockSummaryAddRaw.returnsThis()
+    sinon.stub(core.summary, 'addRaw').callsFake(mockSummaryAddRaw)
+    sinon.stub(core.summary, 'write').callsFake(mockSummaryWrite)
+  }
+  if (setFailed != undefined) {
+    sinon.stub(core, 'setFailed').callsFake(setFailed)
+  }
 }
 
 export interface IssuesRest {
-  listComments: sinon.SinonStub;
-  createComment: sinon.SinonStub;
-  updateComment: sinon.SinonStub;
+  listComments: sinon.SinonStub
+  createComment: sinon.SinonStub
+  updateComment: sinon.SinonStub
 }
 
 export interface ReactionsRest {
-  listForIssue: sinon.SinonStub;
-  deleteForIssue: sinon.SinonStub;
-  createForIssue: sinon.SinonStub;
+  listForIssue: sinon.SinonStub
+  deleteForIssue: sinon.SinonStub
+  createForIssue: sinon.SinonStub
 }
 
 export interface ChecksRest {
-  listForRef: sinon.SinonStub;
-  create: sinon.SinonStub;
-  update: sinon.SinonStub;
+  listForRef: sinon.SinonStub
+  create: sinon.SinonStub
+  update: sinon.SinonStub
 }
 
 export function setupGithubMock(
+  contextToUse: Context.Context = getDefaultGithubContext(),
   issues: IssuesRest = {
     listComments: sinon.stub().resolves({ data: [] }),
     createComment: sinon.stub().resolves(undefined),
@@ -63,8 +62,7 @@ export function setupGithubMock(
     listForRef: sinon.stub().resolves({ data: { check_runs: [] } }),
     create: sinon.stub().resolves(undefined),
     update: sinon.stub().resolves(undefined)
-  },
-  contextToUse: Context.Context
+  }
 ) {
   const mockOctokitClient = {
     rest: {
@@ -72,9 +70,9 @@ export function setupGithubMock(
       reactions: reactions,
       checks: checks,
     },
-  };
-  sinon.stub(github, 'getOctokit').returns(mockOctokitClient as any);
-  sinon.stub(github, 'context').get(() => contextToUse);
+  }
+  sinon.stub(github, 'getOctokit').returns(mockOctokitClient as any)
+  sinon.stub(github, 'context').get(() => contextToUse)
 }
 
 export function getDefaultGithubContext(): Context.Context {
--- scan/integrationTests/src/matrix.integration.ts
+++ scan/integrationTests/src/matrix.integration.ts
@@ -1,163 +1,255 @@
 import * as core from '@actions/core'
-import {main, setFailed} from '../../src/main.function'
-import * as sinon from 'sinon'
-import {execSpy, setupGithubMock, getDefaultGithubContext} from './github.mock.integration'
-import {QODANA_SARIF_NAME} from '../../../common/qodana'
+import {main} from '../../src/main.function'
+import {cacheRestoreSpy, execSpy, setupGithubCoreMock, setupGithubMock} from './github.mock.integration'
+import {EXECUTABLE, FAIL_THRESHOLD_OUTPUT, QODANA_SARIF_NAME, QodanaExitCode, VERSION} from '../../../common/qodana'
 import path from 'path'
 import * as fs from 'fs'
+import {getInputs} from "../../lib/utils"
+import {assert, expect} from 'chai'
+import {LINTER_DATA} from "./constants"
+import * as sinon from "sinon"
+import {Run} from "sarif"
+import {inspect} from "util";
+import Sinon from "sinon";
 
+type LinterCode = keyof typeof LINTER_DATA
+type AnalysisMode = 'native' | 'docker'
 
-type LinterCode = 'QDAND' | 'QDANDC' | 'QDPHP' | 'QDJS' | 'QDNET' | 'QDCPP' | 'QDNETC' | 'QDPY' | 'QDPYC' | 'QDGO' | 'QDJVM' | 'QDJVMC' | 'QDCLC' | 'QDRUBY'
-
-type InputArgs = {
+type TestInputs = {
   os: string,
-  mode: string,
+  mode: AnalysisMode,
   linter: LinterCode,
+  analysisDir: string,
   prMode: boolean,
   useCaches: boolean,
-  withFixes: string,
-  onlySarifGen: boolean
+  fixesMode: string,
+  overrideSarif: boolean
 }
 
-// function getLinterImage(linterCode: LinterCode): string {
-//   return `jetbrains/qodana-${linterCode}:2025.1`;
-// }
-//
-// function getLinterDirectory(linterCode: LinterCode): string {
-//   switch (linterCode) {
-//     case 'python':
-//     case 'python-community':
-//       return 'py_error';
-//     case 'dotnet':
-//     case 'cdnet':
-//       return 'dotnet';
-//     case 'cpp':
-//       return 'cpp';
-//     case 'clang':
-//       return 'cnova';
-//     case 'js':
-//       return 'js';
-//     case 'go':
-//       return 'go';
-//     case 'ruby':
-//       return 'ruby';
-//     case 'php':
-//       return 'duplicates';
-//     case 'jvm':
-//     case 'jvm-community':
-//     case 'android':
-//     case 'jvm-android':
-//     default:
-//       return 'java';
-//   }
-// }
-
-// Run the integration test for a specific matrix combination
 export async function runMatrixTest(
-  matrix: InputArgs
+  matrix: TestInputs
 ): Promise<void> {
+  const setFailedStub: sinon.SinonStub<[message: string | Error], void> = sinon.stub()
+  setupGithubCoreMock(undefined, setFailedStub)
+  setupGithubMock()
 
-  const issues = {
-    listComments: sinon.stub().resolves({ data: [] }),
-    createComment: sinon.stub().resolves(undefined),
-    updateComment: sinon.stub().resolves(undefined)
-  };
+  try {
+    core.info("Starting main function")
+    await main()
 
-  const reactions = {
-    listForIssue: sinon.stub().resolves({ data: [] }),
-    deleteForIssue: sinon.stub().resolves(undefined),
-    createForIssue: sinon.stub().resolves(undefined)
-  };
+    // for debug purposes
+    execSpy.getCalls().forEach(call => {
+      const filteredArgs = call.args.filter(arg => arg != undefined)
+      core.info(`Got call with arguments ${filteredArgs}`)
+    })
 
-  const checks = {
-    listForRef: sinon.stub().resolves({ data: { check_runs: [] } }),
-    create: sinon.stub().resolves(undefined),
-    update: sinon.stub().resolves(undefined)
-  };
+    checkInputs(matrix)
 
-  const defaultContext = getDefaultGithubContext();
-  setupGithubMock(issues, reactions, checks, defaultContext);
-  core.info("Starting main function")
-  await main();
-
-  execSpy.getCalls().forEach(call => {
-    core.info(`Got call with arguments ${call.args}`)
-  })
-  checkInputs(matrix)
-
-  //const resultsDir = getInputsSpy.returnValues[0].resultsDir
-  const resultsDir = process.env.RUNNER_TEMP ? `${process.env.RUNNER_TEMP}/qodana/results` : '/tmp/qodana/results'
-  const sarifSourcePath = path.join(resultsDir, QODANA_SARIF_NAME);
-  if (matrix.onlySarifGen) {
-    saveSarif(matrix, sarifSourcePath)
+    await Promise.all([
+      checkQodanaCalls(matrix),
+      checkStatus(setFailedStub),
+      checkCache(),
+      checkSarifAndMaybeOverride(matrix)
+    ])
+  } finally {
+    cleanup()
   }
-
-  cleanup()
 }
 
 function cleanup() {
-  execSpy.resetHistory()
-  //getInputsSpy.resetHistory()
+  Sinon.restore()
 }
 
-function checkInputs(matrix: InputArgs) {
+function checkInputs(matrix: TestInputs) {
   const expectedInputs = {
-    // args: [],
-    // resultsDir: process.env.RUNNER_TEMP ? `${process.env.RUNNER_TEMP}/qodana/results` : '/tmp/qodana/results',
-    // cacheDir: process.env.RUNNER_TEMP ? `${process.env.RUNNER_TEMP}/qodana/cache` : '/tmp/qodana/cache',
-    // primaryCacheKey: 'qodana-cache',
-    // additionalCacheKey: '',
-    // cacheDefaultBranchOnly: false,
-    // uploadResult: true,
-    // uploadSarif: false,
-    // artifactName: 'qodana-report',
     useCaches: matrix.useCaches,
     useAnnotations: false,
     prMode: matrix.prMode,
     postComment: false,
-    // githubToken: 'fake-token',
-    // pushFixes: 'none',
-    // commitMessage: 'Qodana fixes',
-    // useNightly: false
-  };
-
-  // assert(
-  //   getInputsSpy.returned(sinon.match(expectedInputs)),
-  //   `The input args for Qodana action mismatched. Expected:\n${expectedInputs}\nmatrix: ${matrix}`
-  // )
-}
-
-function assert(check: boolean, message: string) {
-  if (!check) {
-    setFailed(message)
+    pushFixes: 'none',
+    useNightly: false,
   }
+  const actual = getInputs()
+  const errorMessage =
+    `The input args for Qodana action mismatched. Expected:${inspect(expectedInputs)}\nGot: ${inspect(actual)}`
+  expect(actual, errorMessage).to.include(expectedInputs)
 }
 
-function saveSarif(matrix: InputArgs, sarifSourcePath: string) {
-  if (fs.existsSync(sarifSourcePath)) {
-    const sarifDestDir = path.join(__dirname, '../integrationTests/testData');
-    const sarifFileName = `qodana_${matrix.os}_${matrix.mode}_${matrix.linter}_pr-${matrix.prMode}_cache-${matrix.useCaches}_fixes-${matrix.withFixes}.sarif.json`;
-    const sarifDestPath = path.join(sarifDestDir, sarifFileName)
-    if (!fs.existsSync(sarifDestDir)) {
-      fs.mkdirSync(sarifDestDir, { recursive: true })
-    }
-    fs.copyFileSync(sarifSourcePath, sarifDestPath);
-    console.log(`Saved SARIF file to ${sarifDestPath}`);
+async function checkQodanaCalls(matrix: TestInputs) {
+  if (matrix.mode == 'native') {
+    checkQodanaNativeCalls(matrix)
   } else {
-    console.warn(`SARIF file not found at ${sarifSourcePath}`);
+    checkQodanaDockerCalls(matrix)
   }
+}
+
+function checkQodanaNativeCalls(matrix: TestInputs) {
+  const calls = execSpy
+    .getCalls()
+    .filter(call => call.args[0] == EXECUTABLE)
+    .map(call => call.args)
+
+  const errorMessage = `Qodana should be called once in native mode. Actual calls:\n${calls.join('\n')}`
+  assert(execSpy.calledOnce, errorMessage)
+
+  const [, scanArgs] = calls[0]
+  checkArgs(scanArgs, getExpectedScanArgs(matrix))
+}
+
+function getLinterImageWithoutVersion(linterCode: LinterCode): string {
+  return `jetbrains/qodana-${LINTER_DATA[linterCode].linter}`
+}
+
+function checkQodanaDockerCalls(matrix: TestInputs) {
+  const calls = execSpy
+    .getCalls()
+    .filter(call => call.args[0] == EXECUTABLE)
+    .map(call => call.args)
+
+  const errorMessage = `Qodana should be called twice in docker mode. Actual calls:\n${calls.join('\n')}`
+  assert(execSpy.calledTwice, errorMessage)
+
+  const [, pullArgs] = calls[0]
+  const expectedPullArgs = [
+    `pull`,
+    `-l,${getLinterImageWithoutVersion(matrix.linter)}`
+  ]
+  checkArgs(pullArgs, expectedPullArgs)
+
+  const [, scanArgs] = calls[1]
+  checkArgs(scanArgs, getExpectedScanArgs(matrix))
+}
+
+function getExpectedScanArgs(matrix: TestInputs): string[] {
+  const inputs = getInputs()
+  const expectedArgs: string[] = [
+    'scan',
+    `-i,${matrix.analysisDir}`,
+    `--cache-dir,${inputs.cacheDir}`,
+    `--results-dir,${inputs.resultsDir}`
+  ]
+  if (matrix.fixesMode != 'none') {
+    expectedArgs.push(matrix.fixesMode)
+  }
+  if (matrix.prMode) {
+    const hash = process.env.QODANA_PR_SHA
+    expect(hash, `Empty QODANA_PR_SHA for analysis in pr-mode`).not.empty
+    expectedArgs.push(`--commit,${hash}`)
+  }
+  if (matrix.mode == 'native') {
+    expectedArgs.push(`--ide,${matrix.linter}`)
+  } else {
+    expectedArgs.push(`--linter,${getLinterImageWithoutVersion(matrix.linter)}`)
+  }
+  return expectedArgs
+}
+
+function checkArgs(actual: string[] | undefined, expected: string[]) {
+  const argsAsString = actual?.join(',') ?? ''
+  expected.forEach(arg => {
+    expect(argsAsString).contain(arg, `Could not find argument ${arg} in execution: ${argsAsString}`)
+  })
+}
+
+async function checkStatus(setFailedStub: sinon.SinonStub<[message: string | Error], void>) {
+  const qodanaExecutions = await Promise.all(
+    execSpy
+      .getCalls()
+      .filter(call => call.args[0] == EXECUTABLE)
+      .map(call => call.returnValue)
+  )
+  const failedExecution =
+    qodanaExecutions.find(execution => !Object.values(QodanaExitCode).includes(execution.exitCode))
+  if (failedExecution) {
+    throw Error(`Qodana execution resulted in exit code ${failedExecution.exitCode}: ${failedExecution.stderr}\n${failedExecution.stdout}`)
+  }
+
+  const failedByThresholdExecution =
+    qodanaExecutions.find(execution => execution.exitCode == QodanaExitCode.FailThreshold)
+  if (failedByThresholdExecution) {
+    assert(setFailedStub.calledWith(FAIL_THRESHOLD_OUTPUT), `Qodana exited with exit code ${QodanaExitCode.FailThreshold}, but error message was not shown`)
+  }
+}
+
+async function checkCache() {
+  const inputs = getInputs()
+  if (inputs.useCaches) {
+    assert(cacheRestoreSpy.calledOnce, `Qodana tried to restore caches twice`)
+    const returnValue = await cacheRestoreSpy.firstCall.returnValue
+    const errorMessage = `Cache with primary key ${inputs.primaryCacheKey} was not loaded during execution. Please check if you changed cache key`
+    expect(returnValue, errorMessage).not.undefined
+  }
+}
+
+function getSarifName(matrix: TestInputs) {
+  return `qodana_${matrix.os}_${matrix.mode}_${matrix.linter}_pr-${matrix.prMode}_cache-${matrix.useCaches}_fixes-${matrix.fixesMode}.sarif.json`
+}
+
+function getSarifSourcePath() {
+  return path.join(getInputs().resultsDir, QODANA_SARIF_NAME)
+}
+
+async function checkSarifAndMaybeOverride(matrix: TestInputs) {
+  const sarifSourcePath = getSarifSourcePath()
+  const overrideSarif = matrix.overrideSarif
+  if (fs.existsSync(sarifSourcePath)) {
+    const sarifDestDir = path.join(__dirname, '../integrationTests/testData')
+    const sarifDestPath = path.join(sarifDestDir, getSarifName(matrix))
+    if (overrideSarif) {
+      if (!fs.existsSync(sarifDestDir)) {
+        fs.mkdirSync(sarifDestDir, { recursive: true })
+      }
+      fs.copyFileSync(sarifSourcePath, sarifDestPath)
+    } else {
+      try {
+        compareSarifFiles(sarifSourcePath, sarifDestPath)
+      } catch (e) {
+        core.warning("To regenerate sarif files content, launch workflow manually with parameter `override-sarif` set to `true`")
+        throw e
+      }
+    }
+  } else {
+    throw Error(`SARIF file not found at ${sarifSourcePath}`)
+  }
+}
+
+export function compareSarifFiles(actual: string, expected: string) {
+  core.info(`Comparing two sarif files: ${actual} and ${expected}`)
+  const actualContent = fs.readFileSync(actual, 'utf-8')
+  const expectedContent = fs.readFileSync(expected, 'utf-8')
+
+  const actualRun: Run = JSON.parse(actualContent).runs?.[0]
+  const expectedRun: Run = JSON.parse(expectedContent).runs?.[0]
+
+  assert(!actualRun || !expectedRun, `One or both SARIF files do not contain a valid run object.`)
+  assert(JSON.stringify(actualRun.properties) !== JSON.stringify(expectedRun.properties), 'Properties are not the same')
+
+  const actualResults = actualRun.results ?? []
+  const expectedResults = expectedRun.results ?? []
+
+  assert(actualResults.length !== expectedResults.length, `The results number mismatched: expected ${expectedResults.length}, got ${actualResults.length}`)
+
+  const resultsActualSorted = actualResults.map(r => JSON.stringify(r)).sort()
+  const resultsExpectedSorted = expectedResults.map(r => JSON.stringify(r)).sort()
+
+  assert(JSON.stringify(resultsActualSorted) !== JSON.stringify(resultsExpectedSorted), `results mismatch: ${actualResults}\nexpected:\n${expectedResults}`)
+}
+
+function getMatrix(): TestInputs {
+  const matrixJson = process.env.MATRIX_JSON
+  core.info(`Test args: ${matrixJson}`)
+  if (matrixJson == undefined) {
+    throw Error("Could not read input arguments from process.env.MATRIX_JSON")
+  }
+  return JSON.parse(matrixJson)
 }
 
 async function runTest(): Promise<void> {
-  const matrixJson = process.env.MATRIX_JSON;
-  core.info(`got ${matrixJson}`)
-  if (matrixJson == undefined) {
-    setFailed("Could not read input arguments from process.env.MATRIX_JSON")
-    return
+  try {
+    await runMatrixTest(getMatrix())
+  } catch (e) {
+    core.setFailed((e as Error).message)
   }
-  const matrix: InputArgs = JSON.parse(matrixJson);
-  matrix.onlySarifGen = true
-  await runMatrixTest(matrix);
 }
 
-void runTest();
+void runTest()
